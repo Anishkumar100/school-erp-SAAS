baca687bb332dac664b2570b0d0296529bb3448b
AnishKumar100 <akcoder1102004@gmail.com>
2025-08-25 20:21:27 +0530

    I modified cors at server.js

--- api/server.js
+++ api/server.js
@@ -24,7 +24,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {exposedHeaders:"Authorization"}
+const allowedOrigins = [
+  "https://spark-erp-one.vercel.app",
+  "http://localhost:3000" // for local dev
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  exposedHeaders: ["Authorization"]
+};
+
 app.use(cors(corsOptions));
 
 // MONGODB CONNECTION
