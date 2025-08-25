c776bc16b37431332ef8dcdc4d6b3abd19a23f52
AnishKumar100 <akcoder1102004@gmail.com>
2025-08-25 23:18:13 +0530

    fixed bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import axios from 'axios'
-import { AuthProvider } from './context/AuthContext.jsx'
-axios.interceptors.request.use((request)=>{
-  if(localStorage.getItem("token")){
-    request.headers.Authorization = localStorage.getItem("token")
-  }
-  return request;
-})
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+// No need to import axios here anymore for the interceptor
+import { AuthProvider } from './context/AuthContext.jsx';
+
+// The interceptor code has been removed from this file.
+// All authenticated API calls should use the 'apiClient.js' instance instead.
+
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-)
+  <StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
